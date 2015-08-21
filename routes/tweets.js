b5e465c07258550acdfc90ca5bb5841074ae9abb
Steven Follis <steven_follis@live.com>
2015-08-21 13:40:34 -0400

    Initial reset

---
+++ routes/tweets.js
@@ -0,0 +1,47 @@
+﻿var express = require('express');
+var router = express.Router();
+var Twit = require('twit');
+var config = require('../config');
+
+// setup the Twit module 
+var T = new Twit({
+    consumer_key: config.consumer_key,
+    consumer_secret: config.consumer_secret,
+    access_token: config.access_token,
+    access_token_secret: config.access_token_secret
+});
+
+// define an array to hold all tweets
+var tweets = [];
+
+// query the Twitter API with the Twit module
+T.get('search/tweets', { q: '"go to work" from:jaybilas', count: 100, result_type: 'mixed' }, function (err, data, response) {
+    
+    // store the tweets
+    tweets = data.statuses;
+    
+    console.log('# of Tweets: ' + data.statuses.length);
+
+});
+
+/* GET tweets */
+router.get('/tweets', function (request, response) {
+
+    // return the array of tweets
+    response.json(tweets);
+
+});
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,18 @@
+﻿var port = process.env.port || 1337;
+var express = require('express');
+var path = require('path');
+var tweets = require('./routes/tweets.js');
+
+// initialize application
+var app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+
+// setup api routes
+app.use('/api', tweets)
+
+// start the server
+var server = app.listen(port, function () {
+    
+    console.log('GottaGoToWork app listening on port ' + port);
+
+});
