b88c3a577fdf6f1379390aa3bdba62b7da1831e7
Steven Follis <steven_follis@live.com>
2015-08-21 13:44:50 -0400

    Initial reset

---
+++ config.sample.js
@@ -0,0 +1,11 @@
+﻿// The Twitter API requires a developer account and an app created
+// First log into https://dev.twitter.com/ and create an app
+// Then fill in the values below
+// These values are used by Twit to connect Node w/the Twitter API
+// Finally, rename file, removing ".sample" from the filename
+module.exports = {
+    consumer_key: 'XXXXXXXXXXXXXXXXXXXXXX',
+    consumer_secret: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    access_token: 'XXXXXXXX-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    access_token_secret: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+};;
